b9d8eae7c869ccc96c61c0d410ae9925445ab2f9
Ced Ambal <justineambal57@gmail.com>
2025-11-05 18:09:56 +0800

    Web improvement 3.1

--- src/app/event/[eventId]/page.tsx
+++ src/app/event/[eventId]/page.tsx
@@ -3071,7 +3071,7 @@ RECOMMENDATIONS:
           {/* Right Sidebar - Green Rectangle Concept (hidden when chat open) */}
           <div className="lg:col-span-1">
             {!showChat && (
-            <div className="lg:sticky lg:top-8 h-fit space-y-6 z-20 self-start">
+            <div className="lg:sticky lg:top-8 lg:max-h-[calc(100vh-2rem)] lg:overflow-auto h-fit space-y-6 z-20 self-start">
               {/* Event Actions */}
               <div className="bg-green-500/10 border border-green-500/20 rounded-lg p-6 flex flex-col">
                 <h3 className="text-xl font-semibold text-green-400 mb-6">
